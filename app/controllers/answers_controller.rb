ee8dcd8118121c6a16028a5c4691bef3d55fe3ec
Thor Kell <thor@tide-pool.ca>
2016-12-03 15:34:33 -0500

    to i, not to i

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,7 +8,7 @@ class AnswersController < ApplicationController
     # I am going to increment the offset on the client side, because I am a baller.
     # That will fuck up a bit when we run out of answers,
     # but that is enough of an edge case that I am not going to worry about it for now
-    offset  = params[:offset].to_1 || 0
+    offset  = params[:offset].to_i || 0
     @answers = Answer.where(approved: true).limit(5).offset(offset)
     render json: @answers
   end
