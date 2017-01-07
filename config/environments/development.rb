07307a83490c54860e541ee2e6f790e0997751b1
Thor Kell <thor@tide-pool.ca>
2017-01-07 22:54:32 +0000

    Reading from postgres, production env working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,12 +28,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-
   config.action_mailer.perform_caching = false
-
-  # send mails
   config.action_mailer.perform_deliveries = true
-
   # mail details
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
